af0b5b68e19cde8e12309a7a8021050c1617cfbd
subin <subin9033@gmail.com>
2021-07-30 22:21:50 +0900

    make it better

---
+++ route/rank.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import { resolve } from 'path';
+const basePath = resolve();
+const filename = {
+    rank : resolve(basePath,'./db.json')
+}
+
+export const getRank = () => {
+    try {
+        return JSON.parse(fs.readFileSync(filename.rank, 'utf-8'))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export const setRank = (data) => {
+    try {
+        return fs.writeFileSync(filename.rank, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
